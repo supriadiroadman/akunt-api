4054cfa64a48b77663d930bb92e4a8926a71598d
Supriadi Roadman <supriadiroadman@gmail.com>
2022-07-09 22:25:55 +0700

    Finish

---
+++ routes/api.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\TransactionController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::get('/transactions', [TransactionController::class, 'index']);
+// Route::post('/transactions', [TransactionController::class, 'store']);
+// Route::get('/transactions/{id}', [TransactionController::class, 'show']);
+// Route::put('/transactions/{id}', [TransactionController::class, 'update']);
+// Route::delete('/transactions/{id}', [TransactionController::class, 'destroy']);
+
+Route::apiResource('transactions', TransactionController::class);
